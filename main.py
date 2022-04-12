b7797762cb671a367333fed61c1d09d7869bd2b1
maxim.irmscher <maxim.irmscher@idealo.de>
2022-04-12 15:27:25 +0200

    check_result logic added

--- main.py
+++ main.py
@@ -18,8 +18,44 @@ def print_board(board):
     print("   -----------")
 
 
-def check_result():
-    return False
+def check_board(board):
+    if board[0][0] == board[0][1] == board[0][2]:
+        return board[0][0]
+    if board[1][0] == board[1][1] == board[1][2]:
+        return board[1][0]
+    if board[2][0] == board[2][1] == board[2][2]:
+        return board[2][0]
+    if board[0][0] == board[1][0] == board[2][0]:
+        return board[0][0]
+    if board[0][1] == board[1][1] == board[2][1]:
+        return board[0][1]
+    if board[0][2] == board[1][2] == board[2][2]:
+        return board[0][2]
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][1]
+    else:
+        return 0
+
+
+def check_draw(board):
+    draw = True
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == 0:
+                draw = False
+    return draw
+
+
+def check_result(board):
+    winner = check_board(board)
+    if winner != 0:
+        print("player " + str(winner) + " won")
+        exit()
+    elif check_draw(board):
+        print("its a draw")
+        exit()
 
 
 def validate_input(player_input: int):
@@ -46,12 +82,13 @@ def placing(player):
             print("spot not empty")
 
 
-
-
+print_board(gaming_board)
 while True:
-    print_board(gaming_board)
     placing(1)
     print_board(gaming_board)
+    check_result(gaming_board)
     placing(2)
+    print_board(gaming_board)
+    check_result(gaming_board)
 
-    exit()
+

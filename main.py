da6e8cbe638b7462cc4ca6ef9b82c0043974b842
maxim.irmscher <maxim.irmscher@idealo.de>
2022-04-12 19:06:15 +0200

    bugfix

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def check_victory(board):
     if board[0][0] == board[1][1] == board[2][2]:
         return board[0][0]
     if board[0][2] == board[1][1] == board[2][0]:
-        return board[0][1]
+        return board[0][2]
     else:
         return 0
 
